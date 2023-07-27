d0bc25b36b94d4872b375733af9d9a5626803b54
Marek Krug <krug.mrk@gmail.com>
2023-07-27 22:27:01 +0200

    _

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let currentLetter
 let wordsArrayLength = wordsArray.length
 let shownWord = []
 let temp = false
+let temp2 = false
+
 
 console.log(wordsArrayLength)
 
@@ -29,20 +31,29 @@ console.log(chosenWordPos + " " + chosenWord + " und " + chosenWordLength + " la
 
 function checkIfRight(currentLetter) {
     //Check if it was already pressed right
-    for (let i = 0; i < correctLetters.length; i2++) {
-        if(currentLetter = correctLetters[i]) {
+    for (let i = 0; i < correctLetters.length; i++) {
+        if(currentLetter === correctLetters[i]) {
             return true
         }
     }
     //Check if it was already pressed wrong
-
-    //Check the Button if right
+    for (let i = 0; i < wrongLetters.length; i++) {
+        if(currentLetter === wrongLetters[i]) {
+            return true
+        }
+    }
+    //Check the Button if not already pressed
+    let temp2 = false
     for (let i = 0; i < chosenWordLength; i++) {
         if (chosenWord[i] == currentLetter) {
             console.log("richtig!")
             correctLetters.push(currentLetter)
+            temp2 = true
         }
         console.log("ok")
+    }
+    if (temp2 === false) {
+        wrongLetters.push(currentLetter)
     }
 }
 
@@ -50,7 +61,7 @@ function refreshShownWord () {
     shownWord = []
     for (let i = 0; i < chosenWordLength; i++) {
         for (let i2 = 0; i2 < correctLetters.length; i2++) {
-            if(chosenWord[i] = correctLetters[i2]) {
+            if(chosenWord[i] === correctLetters[i2]) {
                 temp = true
             }
         }
@@ -93,17 +104,77 @@ function drawInterface() {
     text(wrongLettersCounter + ' Falsch: ' + wrongLetters, 10, 65)
     fill('white')
     text(shownWord, 10, 85)
-    
 }
 
+function drawKeyboard() {
+    const keyboardLayout = [
+        ["q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "ü", " ",],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä", " "],
+        [" ", "y", "x", "c", "v", "b", "n", "m", " ", " ", " "],
+    ];
+
+    const keyWidth = 20;
+    const keyHeight = 20;
+    const startX = 0;
+    const startY = 120;
+
+    for (let row = 0; row < keyboardLayout.length; row++) {
+        for (let col = 0; col < keyboardLayout[row].length; col++) {
+            const keyLabel = keyboardLayout[row][col];
+            const x = startX + col * keyWidth;
+            const y = startY + row * keyHeight;
+            let temp3 = false;
+            // Check if it was pressed right
+            for (let i = 0; i < correctLetters.length; i++) {
+                if (keyLabel == correctLetters[i]) {
+                    temp3 = true;
+                    break; // No need to check further if the letter is found
+                }
+            }
+            // Check if it was pressed wrong
+            if (!temp3) {
+                for (let i = 0; i < wrongLetters.length; i++) {
+                    if (keyLabel == wrongLetters[i]) {
+                        temp3 = true;
+                        break; // No need to check further if the letter is found
+                    }
+                }
+            }
+
+            // Set the color based on the check result
+            if (temp3) {
+                fill(50);
+            } else {
+                fill(255);
+            }
+
+            stroke(0);
+            rect(x, y, keyWidth, keyHeight);
+
+            fill(0);
+            textSize(16);
+            textAlign(CENTER, CENTER);
+            text(keyLabel, x + keyWidth / 2, y + keyHeight / 2);
+        }
+    }
+}
+
+
 function setup() {
-    createCanvas(300, 200)
+    createCanvas(500, 500)
     drawInterface()
     frameRate(2)
 }
 
 function draw() {
-    drawInterface()
-}
+    clear();
+    push(); // Speichere das aktuelle Koordinatensystem
+    drawInterface();
+    translate(10, 0); // Verschiebe das Koordinatensystem um 10 Pixel nach rechts
 
-// temp
+    drawKeyboard();
+
+
+
+    pop(); // Setze das Koordinatensystem zurück
+}
